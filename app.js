323d49d66eeac75634176a53cbb0cd60e04d0944
mark-scholes <innominatus15@outlook.com>
2020-06-05 04:35:46 +0100

    updated the app so the h1 and p tags show the correct month and full date dynamically

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("beep");
+const date = new Date();
+const month = date.getMonth();
+const year = date.getFullYear();
+const weekday = date.getDay();
+const day = date.getDate() <= 9 ? "0" + date.getDate() : date.getDate();
+const months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentMonth = months[month];
+const dayOfTheWeek = weekdays[weekday];
+document.querySelector(".day h1").innerHTML = currentMonth;
+document.querySelector(".day p").innerHTML =
+  dayOfTheWeek + " " + day + " " + currentMonth + " " + year;
+
+// it would be easier to use toDateString but this puts the date after the month and shortens the weekday
